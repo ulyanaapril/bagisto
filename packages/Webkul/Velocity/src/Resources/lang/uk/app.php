1983dd1614159c1d29333a1511b2167f81428ce4
ulyanadatser <ulyanaapril@gmail.com>
2021-03-16 17:33:08 +0200

    переклад

---
+++ packages/Webkul/Velocity/src/Resources/lang/uk/app.php
@@ -0,0 +1,313 @@
+<?php
+
+return [
+    'admin' => [
+        'system' => [
+            'velocity' => [
+                'general' => 'Загальне',
+                'category' => 'Категорія',
+                'settings' => 'Налаштування',
+                'extension_name' => 'Тема Velocity',
+                'error-module-inactive' => 'Попередження: статус теми Velocity неактивний',
+            ],
+
+            'settings' => [
+                'channels' => [
+                    'subscription_bar' => 'Вміст панелі підписки'
+                ],
+            ],
+
+            'channels' => [
+                'status' => 'Статус',
+                'active' => 'Активний',
+                'inactive' => 'Неактивний',
+            ],
+            'category' => [
+                'all' => 'Всі',
+                'left' => 'Ліворуч',
+                'right' => 'Право',
+                'active' => 'Активний',
+                'custom' => 'Custom',
+                'inactive' => 'Неактивний',
+                'image-alignment' => 'Вирівнювання зображення',
+                'icon-status' => 'Статус піктограми категорії',
+                'image-status' => 'Статус зображення категорії',
+                'sub-category-show' => 'Показати підкатегорію',
+                'image-height' => 'Висота зображення [у пікселях]',
+                'image-width' => 'Ширина зображення [у пікселях]',
+                'show-tooltip' => 'Показати підказку категорії',
+                'num-sub-category' => 'Кількість підкатегорії',
+            ]
+        ],
+        'layouts' => [
+            'velocity' => 'Velocity',
+            'cms-pages' => 'Сторінки CMS',
+            'meta-data' => 'Метадані',
+            'category-menu' => 'Меню категорій',
+            'header-content' => 'Зміст заголовка',
+        ],
+        'contents' => [
+            'self' => 'Self',
+            'active' => 'Активний',
+            'new-tab' => 'Нова вкладка',
+            'inactive' => 'Неактивний',
+            'title' => 'Список вмісту',
+            'select' => '- Виберіть -',
+            'add-title' => 'Додати вміст',
+            'btn-add-content' => 'Додати вміст',
+            'save-btn-title' => 'Зберегти вміст',
+            'autocomplete' => '[Автозаповнення]',
+            'no-result-found' => 'Запису не знайдено.',
+            'search-hint' => 'Шукати продукт тут ...',
+            'mass-delete-success' => 'Вибраний вміст успішно видалено.',
+            'tab' => [
+                'page' => 'Налаштування сторінки',
+                'content' => 'Налаштування вмісту',
+                'meta_content' => 'Метадані',
+            ],
+            'page' => [
+                'title' => 'Заголовок',
+                'status' => 'Статус',
+                'position' => 'Позиція',
+            ],
+            'content' => [
+                'content-type' => 'Тип вмісту',
+                'custom-title' => 'Спеціальний заголовок',
+                'category-slug' => 'Категорія Slug',
+                'link-target' => 'Ціль посилання на сторінку',
+                'custom-product' => 'Зберігати продукти',
+                'custom-heading' => 'Спеціальний заголовок',
+                'catalog-type' => 'Тип каталогу продукту',
+                'static-description' => 'Опис вмісту',
+                'page-link' => 'Посилання на сторінку [наприклад http://example.com/../../] ',
+            ],
+            'datagrid' => [
+                'id' => 'Id',
+                'title' => 'Заголовок',
+                'status' => 'Статус',
+                'position' => 'Позиція',
+                'content-type' => 'Тип вмісту',
+            ]
+        ],
+        'meta-data' => [
+            'footer' => 'Нижній колонтитул',
+            'title' => 'Метадані Velocity',
+            'activate-slider' => 'Активувати слайдер',
+            'home-page-content' => 'Вміст домашньої сторінки',
+            'footer-left-content' => 'Заголовок лівого вмісту',
+            'subscription-content' => 'Вміст панелі підписки',
+            'sidebar-categories' => 'Категорії бічної панелі',
+            'header_content_count' => 'Кількість вмісту заголовка',
+            'footer-left-raw-content' => '<p> Ми любимо розробляти програмне забезпечення та вирішувати реальні проблеми за допомогою бінарних файлів. Ми дуже віддані своїм цілям. Ми інвестуємо свої ресурси, щоб створити програмне забезпечення та додатки для корпоративного бізнесу світового класу на найвищому рівні, передовий досвід у галузі технологій. </p> ',
+            'slider-path' => 'Шлях слайдера',
+            'category-logo' => 'Логотип категорії',
+            'product-policy' => 'Товарна політика',
+            'update-meta-data' => 'Оновити метадані',
+            'product-view-image' => 'Product View Image',
+            'advertisement-two' => 'Реклама Два Зображення',
+            'advertisement-one' => 'Реклама Одне Зображення',
+            'footer-middle-content' => 'Середній вміст колонтитула',
+            'advertisement-four' => 'Реклама чотири зображення',
+            'advertisement-three' => 'Реклама Три зображення',
+            'images' => 'Зображення',
+            'general' => 'Загальне',
+            'add-image-btn-title' => 'Додати зображення',
+            'footer-middle' => [
+                'about-us' => 'Про нас',
+                'customer-service' => 'Обслуговування клієнтів',
+                'whats-new' => 'Що нового',
+                'contact-us' => "Зв\'яжіться з нами",
+                'order-and-return' => 'Замовлення та повернення',
+                'Payment-Policy' => 'Політика платежів',
+                'shipping-policy' => 'Політика доставки',
+                'privacy-and-cookies-policy' => 'Політика конфіденційності та файлів cookie'
+            ]
+        ],
+        'category' => [
+            'save-btn-title' => 'Меню збереження',
+            'title' => 'Список меню категорії',
+            'add-title' => 'Додати вміст меню',
+            'edit-title' => 'Редагувати вміст меню',
+            'btn-add-category' => 'Додати вміст категорії',
+            'datagrid' => [
+                'category-id' => 'Ідентифікатор категорії',
+                'category-name' => 'Назва категорії',
+                'category-icon' => 'Піктограма категорії',
+                'category-status' => 'Статус',
+            ],
+            'tab' => [
+                'general' => 'Загальне',
+            ],
+            'status' => 'Статус',
+            'active' => 'Активний',
+            'inactive' => 'Неактивний',
+            'select' => '- Виберіть -',
+            'icon-class' => 'Значок класу',
+            'select-category' => 'Вибрати категорію',
+            'tooltip-content' => 'Вміст підказки',
+            'mass-delete-success' => 'Меню вибраних категорій успішно видалено.',
+        ],
+        'general' => [
+            'locale_logo' => 'Локальний логотип',
+        ],
+    ],
+
+    'home' => [
+        'view-all' => 'Переглянути всі',
+        'add-to-cart' => 'Додати в кошик',
+        'hot-categories' => 'Гарячі категорії',
+        'Payment-methods' => 'Способи оплати',
+        'customer-reviews' => "Відгуки клієнтів",
+        'shipping-methods' => 'Способи доставки',
+        'popular-categories' => 'Популярні категорії',
+    ],
+
+    'header' => [
+        'cart' => 'Кошик',
+        'guest' => 'Гість',
+        'logout' => 'Вийти',
+        'title' => 'Обліковий запис',
+        'account' => 'Обліковий запис',
+        'profile' => 'Профіль',
+        'wishlist' => 'Список бажань',
+        'all-categories' => 'Усі категорії',
+        'search-text' => 'Шукати продукти тут',
+        'welcome-message' => "Ласкаво просимо, :customer_name",
+        'dropdown-text' => 'Керувати кошиком, замовленнями та списком бажань',
+    ],
+
+    'menu-navbar' => [
+        'text-more' => 'Більше',
+        'text-category' => 'Категорії',
+    ],
+
+    'minicart' => [
+        'cart' => 'Кошик',
+        'view-cart' => 'Переглянути кошик',
+    ],
+
+    'checkout' => [
+        'qty' => 'Кількість',
+        'checkout' => 'Оформити замовлення',
+        'cart' => [
+            'view-cart' => 'Переглянути кошик',
+            'cart-summary' => 'Підсумок кошика',
+        ],
+        'items' => 'Предмети',
+        'subtotal' => 'Проміжний підсумок',
+        'sub-total' => 'Загальна сума',
+        'continue' => 'Перейти до оплати',
+    ],
+
+    'customer' => [
+        'compare' => [
+            'text' => 'Порівняти',
+            'compare_s similar_items' => 'Порівняти схожі елементи',
+            'add-tooltip' => 'Додати товар до списку порівняння',
+            'added' => 'Елемент успішно додано до списку порівняння',
+            'already_added' => 'Елемент вже додано до списку порівняння',
+            'remove' => 'Елемент успішно видалено зі списку порівняння',
+            'remove-all' => 'Усі елементи успішно видалено зі списку порівняння',
+            'empty-text' => "У вас немає елементів у вашому списку порівняння",
+            'product_image' => 'Зображення продукту',
+            'actions' => 'Дії',
+        ],
+        'login-form' => [
+            'sign-up' => 'Зареєструватися',
+            'new-customer' => 'Новий клієнт',
+            'customer-login' => 'Вхід клієнта',
+            'registered-user' => 'Зареєстрований користувач',
+            'your-email-address' => 'Ваша електронна адреса',
+            'form-login-text' => 'Якщо у вас є обліковий запис, увійдіть за допомогою своєї адреси електронної пошти.',
+        ],
+        'signup-form' => [
+            'login' => 'Увійти',
+            'become-user' => 'Стати користувачем',
+            'user-registration' => 'Реєстрація користувача',
+            'form-sginup-text' => 'Якщо ви новачок у нашому магазині, ми будемо раді вам стати членом.',
+        ],
+        'forget-password' => [
+            'login' => 'Увійти',
+            'Forgot-Password' => 'Забули пароль',
+            'recovery-password' => 'Відновити Пароль ',
+            'recovery-password-text' => 'Якщо ви забули свій пароль, відновіть його, ввівши свою адресу електронної пошти.',
+        ],
+        'wishlist' => [
+            'remove-all-success' => 'Усі елементи з вашого списку бажань були видалені',
+        ],
+    ],
+
+    'error' => [
+        'go-to-home' => 'Йти додому',
+        'page-lost-short' => 'Втрачений вміст сторінки',
+        'something_went_wrong' => 'щось пішло не так',
+        'page-lost-description' => "Сторінка, яку ви шукаєте, недоступна. Спробуйте здійснити пошук ще раз або скористайтеся кнопкою Назад.",
+    ],
+
+    'products' => [
+        'text' => 'Товари',
+        'details' => 'Подробиці',
+        'reviews-title' => 'Відгуки',
+        'reviewed' => 'Переглянуто',
+        'review-by' => 'Огляд',
+        'quick-view' => 'Швидкий перегляд',
+        'not-available' => 'Недоступно',
+        'submit-review' => 'Надіслати відгук',
+        'ratings' => ':totalRatings Рейтинг',
+        'reviews-count' => ':totalReviews Відгуки',
+        'customer-rating' => 'Рейтинг клієнта',
+        'more-infomation' => 'Додаткова інформація',
+        'view-all-reviews' => 'Переглянути всі відгуки',
+        'write-your-review' => 'Напишіть свій відгук',
+        'short-description' => 'Короткі описи',
+        'recently-viewed' => 'Нещодавно переглянуті товари',
+        'be-first-review' => 'Будьте першим, хто напише відгук',
+    ],
+
+    'shop' => [
+        'gender' => [
+            'male' => 'Чоловік',
+            'other' => 'Інше',
+            'female' => 'Жінка',
+        ],
+        'general' => [
+            'no' => 'Ні',
+            'yes' => 'Так',
+            'view' => 'Переглянути',
+            'filter' => 'Фільтр',
+            'orders' => 'Замовлення',
+            'update' => 'Оновити',
+            'reviews' => 'Відгуки',
+            'download' => 'Завантажити',
+            'currencies' => 'Валюти',
+            'adrese' => 'Адреси',
+            'top-brand' => 'Найкращі бренди',
+            'new-password' => 'Новий пароль',
+            'no-file-available' => 'Файл недоступний!',
+            'downloadables' => 'Завантажувані продукти',
+            'confirm-new-password' => 'Підтвердити новий пароль',
+            'enter-current-password' => 'Введіть ваш поточний пароль',
+
+            'alert' => [
+                'info' => 'Інформація',
+                'error' => 'Помилка',
+                'success' => 'Успіх',
+                'warning' => 'Попередження',
+            ],
+        ],
+        'wishlist' => [
+            'add-wishlist-text' => 'Додати товар у список бажань',
+            'remove-wishlist-text' => 'Видалити продукт зі списку бажань'
+        ]
+    ],
+
+    'responsive' => [
+        'header' => [
+            'done' => 'Готово',
+            'languages' => 'Мови',
+            'pozdrav' => 'Ласкаво просимо, :customer!',
+        ]
+    ],
+]
+
+?>
