7712d2c31bbde66bcf4a5e86dba050c32e39391b
ulyanadatser <ulyanaapril@gmail.com>
2021-06-15 09:03:44 +0300

    test

--- packages/Red/Ukrposhta/src/Database/Migrations/2021_06_15_090116_add_fields_to_addresses_table.php
+++ packages/Red/Ukrposhta/src/Database/Migrations/2021_06_15_090116_add_fields_to_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToAddressesTable extends Migration
+class AddFieldsToAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,10 @@ class AddColumnsToAddressesTable extends Migration
     public function up()
     {
         Schema::table('addresses', function (Blueprint $table) {
-//            $table->string('district', 255)->nullable();
-//            $table->string('street', 255)->nullable();
-//            $table->string('house', 50)->nullable();
-//            $table->string('apartment', 50)->nullable();
+            $table->string('district', 255)->nullable();
+            $table->string('street', 255)->nullable();
+            $table->string('house', 50)->nullable();
+            $table->string('apartment', 50)->nullable();
         });
     }
 
